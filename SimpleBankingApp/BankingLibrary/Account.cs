47f23ebde2ec89813314f769f06a0a5bef4dc8a3
max-student <max-student@maxtrain.com>
2020-02-12 16:32:33 -0500

    real end of day 2/12

---
+++ SimpleBankingApp/BankingLibrary/Account.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BankingLibrary {
+    public class Account {
+        public decimal Balance;
+        public string Number;
+        public string Nickname;
+        public void Deposit(decimal amount) {
+            Balance += amount;
+        }
+        public void Withdraw(decimal amount) {
+            Balance -= amount;
+        }
+        public void Transfer(decimal amount, Account to, Account from) {
+            from.Withdraw(amount);
+            to.Deposit(amount);
+        }
+        public decimal ViewBalance(Account account) {
+            return Balance;
+        }
+    }
+}
---
+++ SimpleBankingApp/SimpleBankingApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using BankingLibrary;
+using System;
+
+namespace SimpleBankingApp {
+    class Program {
+        static void Main(string[] args) {
+            var myAccount = new CheckingAccount() {
+                Nickname = "My Big Account", 
+                Number = "192837465",
+                //Balance = 1000.00
+            };
+            myAccount.Deposit(400);
+            myAccount.Withdraw(20);
+            myAccount.ViewBalance();
+        }
+    }
+}
