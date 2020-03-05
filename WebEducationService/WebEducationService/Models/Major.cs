74772e03e698059e905d1b7bdc26e8b8788b262d
max-student <max-student@maxtrain.com>
2020-03-05 10:46:19 -0500

    mid-day commit 3/5

---
+++ WebEducationService/WebEducationService/Models/Major.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebEducationService.Models {
+    public class Major {
+
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public int MinSAT { get; set; }
+
+        public Major() {}
+
+    }
+}
